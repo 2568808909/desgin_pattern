88626eca142228a4a88361b387d0cccb850af173
2568808909 <2568808909@qq.com>
2020-06-21 08:42:19 +0800

    观察者模式

---
+++ src/main/java/cn/ccb/pattern/behavioral/observer/Question.java
@@ -0,0 +1,14 @@
+package cn.ccb.pattern.behavioral.observer;
+
+public class Question {
+    String username;
+    String content;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
---
+++ src/main/java/cn/ccb/pattern/behavioral/observer/Teacher.java
@@ -0,0 +1,18 @@
+package cn.ccb.pattern.behavioral.observer;
+
+public class Teacher {
+
+    String name;
+
+    public Teacher(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
