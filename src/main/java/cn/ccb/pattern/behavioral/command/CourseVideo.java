955e7f0c1e6a80a0df31b0a42b45e20f1e44d907
2568808909 <2568808909@qq.com>
2020-06-21 11:00:27 +0800

    命令模式

---
+++ src/main/java/cn/ccb/pattern/behavioral/command/CourseVideo.java
@@ -0,0 +1,17 @@
+package cn.ccb.pattern.behavioral.command;
+
+public class CourseVideo {
+    private String name;
+
+    public CourseVideo(String name) {
+        this.name = name;
+    }
+
+    public void open(){
+
+    }
+
+    public void close(){
+
+    }
+}
